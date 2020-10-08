76c672b594eee97c400f3ccc0d3254d44e0f1676
Nadia Kent nkent28@gmail.com <nkent28@gmail.com>
2020-10-09 08:20:10 +1300

    added js code

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -9,7 +9,7 @@ const APIController = (function() {
   const clientId = '';
   const clientSecret = '';
 
-
+  // private methods
   const _getToken = async () => {
 
     const result = await fetch('https://accounts.spotify.com/api/token', {
@@ -90,7 +90,88 @@ const APIController = (function() {
         return _getTrack(token, trackEndpoint);
       }
       }
-    } //CODE IS READY TO PUSH FOR TOMORROW, CHECK GITHUB
+    }
 })();
+
+// UI Module
+const UIController = (function() {
+
+  // object to hold references to html selectors
+  const DOMElements = {
+      selectGenre: '#select_genre',
+      selectPlaylist: '#select_playlist',
+      buttonSubmit: '#btn_submit',
+      divSongDetail: '#song-detail',
+      hfToken: '#hidden_token',
+      divSonglist: '.song-list'
+  }
+
+  // public methods
+  return {
+
+    // method to get input fields
+    inputField() {
+      return {
+      genre: document.querySelector(DOMElements.selectGenre),
+      playlist: document.querySelector(DOMElements.selectPlaylist),
+      songs: document.querySelector(DOMElements.divSonglist),
+      submit: document.querySelector(DOMElements.buttonSubmit),
+      songDetail: document.querySelector(DOMElements.divSongDetail)
+  }
+  },
+
+    // need methods  to create select list option
+    createGenre(text, value) {
+      const html = `<option value="${value}">${text}</option>`;
+      document.querySelector(DOMElements.selectGenre).insertAdjacentHTML(`beforeend`, html);
+    },
+
+    createPlaylist(text, value) {
+      const html = `<option value="${value}">${text}</option>`;
+      document.querySelector(DOMElements.selectPlaylist).insertAdjacentHTML(`beforeend`, html);
+    },
+
+    // need method to create a track list group item
+    createTrack(id, name) {
+      const html = `<a href="#" class="list-group-item list-group-item-action list-group-item-light" id="${id}">${name}</a>`;
+      document.querySelector(DOMElements.divSonglist).insertAdjacentHTML(`beforeend`, html);
+    },
+
+    // need method to create the song detail
+    createSongDetail(img, title, artist) {
+      const detailDiv = document.querySelector(DOMElements.divSongDetail);
+      // any time user clicks a new song, we need to clear out the song detail div
+      detailDiv.innerHTML = '';
+
+      const html =
+      `
+      <div class="row col-sm-12 px-0">
+         <img src="${img}" alt="">
+      </div>
+      <div class="row col-sm-12 px-0">
+          <label for="Genre" class="form-label col-sm-12>${title}:</label>
+      </div>
+      <div class="row col-sm-12 px-0">
+          <label for="artist" class="form-label col-sm-12>"By ${artist}:</label>
+      </div>
+      `;
+
+      detailDiv.innerAdjacentHTML(`beforeend`, html)
+    },
+
+    resetTrackDetail() {
+        this.inputField().songDetail.innerHTML = '';
+    },
+
+    resetTracks() {
+        this.inputField().songs.innerHTML = '';
+        this.inputField().songDetail.innerHTML = '';
+    },
+
+    resetPlaylist() {
+        this.inputField().playlist.innerHTML = '';
+        this.resetTracks();
+     }
+  }
 
 
